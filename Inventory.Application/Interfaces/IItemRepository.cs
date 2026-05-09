4f3b7edf58d0796c74562e78edb1260eec732067
Msekeli <msekeli14@gmail.com>
2026-05-10 00:57:53 +0200

    Add inventory service methods and new exception classes

--- Inventory.Application/Interfaces/IItemRepository.cs
+++ Inventory.Application/Interfaces/IItemRepository.cs
@@ -5,10 +5,20 @@ namespace Inventory.Application.Interfaces;
 public interface IItemRepository
 {
     Task<Item?> GetByIdAsync(int id);
+
     Task<Item?> GetBySKUAsync(string sku);
+
     Task<List<Item>> GetAllAsync();
+
+    Task<List<Item>> GetActiveItemsAsync();
+
+    Task<List<Item>> GetLowStockItemsAsync();
+
     Task AddAsync(Item item);
+
     Task UpdateAsync(Item item);
+
     Task DeleteAsync(Item item);
+
     Task<bool> SaveChangesAsync();
 }
--- Inventory.Application/Interfaces/IStockTransactionRepository.cs
+++ Inventory.Application/Interfaces/IStockTransactionRepository.cs
@@ -4,7 +4,13 @@ namespace Inventory.Application.Interfaces;
 
 public interface IStockTransactionRepository
 {
+    Task<StockTransaction?> GetByIdAsync(int id);
+
     Task<List<StockTransaction>> GetByItemIdAsync(int itemId);
+
+    Task<List<StockTransaction>> GetAllAsync();
+
     Task AddAsync(StockTransaction transaction);
+
     Task<bool> SaveChangesAsync();
 }
